ba871223af68d6357d15833e8c4669bb0fda4178
DevKyle <74616861+DevKyleK@users.noreply.github.com>
2020-12-15 15:12:14 +0000

    Create template.py
    Template for the loopOS code

---
+++ main/template.py
@@ -0,0 +1,20 @@
+#######################
+# LoopStack Template  #
+#######################
+# Code Version 0.0.0  #
+#######################
+# _                   #
+#| | ___   ___  _ __  #
+#| |/ _ \ / _ \| '_ \ #
+#| | (_) | (_) | |_) |#
+#|_|\___/ \___/| .__/ #
+#              |_|    #
+#######################
+
+
+from m5stack import *
+from m5ui import *
+from uiflow import *
+import random
+import time
+setScreenColor(0x222222)
