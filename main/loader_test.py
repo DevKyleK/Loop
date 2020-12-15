cf598e77f79d12fbc48e113bda732f942a0337e9
DevKyle <74616861+DevKyleK@users.noreply.github.com>
2020-12-15 15:26:43 +0000

    Update loader_test.py
    Updated it with a new while loop

--- main/loader_test.py
+++ main/loader_test.py
@@ -38,3 +38,40 @@ wait(1)
 bar2.setBgColor(0xffffff)
 wait(1)
 bar3.setBgColor(0xffffff)
+#######################
+#   Loader Example 1  #
+#######################
+# Code Version 0.0.0  #
+#######################
+# _                   #
+#| | ___   ___  _ __  #
+#| |/ _ \ / _ \| '_ \ #
+#| | (_) | (_) | |_) |#
+#|_|\___/ \___/| .__/ #
+#              |_|    #
+#######################
+from m5stack import *
+from m5ui import *
+from uiflow import *
+import time
+setScreenColor(0x222222)
+bar1 = M5Rect(130, 105, 10, 30, 0xFFFFFF, 0xFFFFFF)
+bar2 = M5Rect(155, 90, 10, 60, 0xFFFFFF, 0xFFFFFF)
+bar3 = M5Rect(180, 105, 10, 30, 0xFFFFFF, 0xFFFFFF)
+bar1.show()
+bar2.show()
+bar3.show()
+run = True
+while run:
+    wait(1)
+    bar1.setBgColor(0x0099ff)
+    wait(1)
+    bar2.setBgColor(0x0099ff)
+    wait(1)
+    bar3.setBgColor(0x0099ff)
+    wait(1)
+    bar1.setBgColor(0xFFFFFF)
+    wait(1)
+    bar2.setBgColor(0xFFFFFF)
+    wait(1)
+    bar3.setBgColor(0xFFFFFF)
